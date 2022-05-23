4544f698a8949d309d87a04a163ab7e7f2dd52a0
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-23 14:13:07 +0600

    Added home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div >
-      <h1 className="text-3xl font-bold">Hello World!</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
