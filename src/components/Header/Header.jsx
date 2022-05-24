5edfbdda25b6a9c13c7fce010726ed5829dcbfe7
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-24 20:43:09 +0600

    made header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../Layout/Container'
 import NavLink from './NavLink/NavLink'
+import { BiMenu } from 'react-icons/bi'
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div>
-      <Container className='h-20 flex items-center'>
+      <Container className='h-20 flex items-center relative'>
         <div className='cursor-pointer'>
           <div>
             <span className='font-bold text-2xl'>COM</span>
@@ -14,7 +16,10 @@ const Header = () => {
           <span className='-mt-2 block text-zinc-500'>best pc parts</span>
         </div>
 
-        <div className='ml-auto flex gap-1'>
+        <div className='ml-auto md:hidden'>
+          <BiMenu className='text-3xl' onClick={() => setOpen(!open)} />
+        </div>
+        <div className={`md:ml-auto flex flex-col w-full gap-1 absolute md:static md:flex-row md:w-auto top-20 left-0 px-4 bg-zinc-300 md:bg-transparent z-50 py-5 ${open ? '': 'hidden md:flex'}`}>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/dashboard">Dashboard</NavLink>
           <NavLink to="/login">Login</NavLink>
