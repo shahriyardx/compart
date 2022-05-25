485c7e5a2d8ec1192d0e7d930f63b94d0c3b1376
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-26 04:00:39 +0600

    Loads poroduct from api

--- src/pages/Dashboard/Products.jsx
+++ src/pages/Dashboard/Products.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useQuery } from "react-query";
 import DashPage from "../../components/Layout/DashPage";
+import { API_BASE } from "../config";
 
 const Products = () => {
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useQuery("products", () =>
+    fetch(`${API_BASE}/products`).then((data) => data.json())
+  );
+
   return (
     <DashPage>
-      <h1 className="text-2xl">All Products : 9</h1>
+      <h1 className="text-2xl">All Products : {products?.length}</h1>
 
       <div className="w-full mt-2 overflow-x-auto">
-        <table class="table-auto text-left w-full">
+        <table className="table-auto text-left w-full">
           <thead className="bg-black text-white">
             <tr>
               <th className="p-3 text-sm font-semibold text-left">Sl.</th>
@@ -18,25 +28,31 @@ const Products = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="even:bg-zinc-200">
-              <td className="p-3 text-sm text-gray-700 whitespace-nowrap">1</td>
-              <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                Malcolm Lockyer
-              </td>
-              <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                500
-              </td>
+            {products?.map((item, index) => {
+              return (
+                <tr className="even:bg-zinc-200">
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {index + 1}
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {item.name}
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {item.quantity}
+                  </td>
 
-              <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                100
-              </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {item.price}
+                  </td>
 
-              <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                <button className="px-3 py-2 bg-red-500 hover:bg-red-600 text-white rounded-lg">
-                  Delete
-                </button>
-              </td>
-            </tr>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <button className="px-3 py-2 bg-red-500 hover:bg-red-600 text-white rounded-lg">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
