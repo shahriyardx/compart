ccccbc4de501600a6e7467fe3da945d73f20255b
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-26 23:49:37 +0600

    Added reviews

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Stars from './Stars'
+import React from "react";
+import Stars from "./Stars";
 
-const Review = () => {
+const Review = ({ data }) => {
   return (
-    <div className='p-4 border-2 rounded-md shadow-sm'>
-      <div className='flex justify-between'>
-        <h1 className='text-xl font-bold text-blue-600 font-mono underline underline-offset-2'>Someone</h1>
+    <div className="p-4 border-2 rounded-md shadow-sm">
+      <div className="flex justify-between">
+        <h1 className="text-lg font-bold text-blue-600 font-mono underline underline-offset-2">
+          {data.name}
+        </h1>
         <Stars />
       </div>
 
-      <div className='mt-5 font-semibold text-zinc-500'>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis itaque laudantium totam tempora impedit deleniti odit fugit incidunt expedita nostrum, soluta fugiat velit? Hic at recusandae neque amet soluta repellendus?</p>
+      <div className="mt-5 font-semibold text-zinc-500">
+        <p>{data.content}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
