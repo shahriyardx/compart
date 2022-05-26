e3f604ff1f7288e0f2a8283cf893b44b3f6c4cde
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-26 16:30:20 +0600

    Functional orders

--- src/pages/Dashboard/Orders.jsx
+++ src/pages/Dashboard/Orders.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import { useQuery } from "react-query";
 import DashPage from "../../components/Layout/DashPage";
@@ -13,6 +14,22 @@ const Orders = () => {
     fetch(`${API_BASE}/order`).then((data) => data.json())
   );
 
+  const updateStatus = async (currentStatus, order_id) => {
+    const statusMap = {
+      Created: "Shipped",
+      Shipped: "Delivered",
+      Cancelled: "Cancelled",
+    };
+
+    const newStatus = { order_id, status: statusMap[currentStatus] };
+    const { data: updateData } = await axios.put(
+      `${API_BASE}/order/update`,
+      newStatus
+    );
+
+    alert(updateData.success);
+  };
+
   return (
     <DashPage>
       <h1 className="text-2xl">All Orders</h1>
@@ -21,15 +38,27 @@ const Orders = () => {
         <table className="table-auto text-left w-full">
           <thead className="bg-black text-white">
             <tr>
-              <th className="p-3 text-sm font-semibold text-left">Sl.</th>
-              <th className="p-3 text-sm font-semibold text-left">Name</th>
-              <th className="p-3 text-sm font-semibold text-left">
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
+                Sl.
+              </th>
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
+                Name
+              </th>
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
+                Quantity
+              </th>
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
+                Total Price
+              </th>
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
                 Payment Status
               </th>
-              <th className="p-3 text-sm font-semibold text-left">
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
                 Shipping Status
               </th>
-              <th className="p-3 text-sm font-semibold text-left">Actions</th>
+              <th className="p-3 text-sm font-semibold whitespace-nowrap text-left">
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -41,6 +70,14 @@ const Orders = () => {
                   </td>
                   <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
                     {order.product_name}
+                  </td>
+
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {order.quantity}
+                  </td>
+
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    ${order.due}
                   </td>
                   <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
                     <span
@@ -57,17 +94,37 @@ const Orders = () => {
                   </td>
 
                   <td className="p-3 text-sm text-gray-700 whitespace-nowrap flex gap-2">
-                    <button className="px-3 py-2 bg-red-500 hover:bg-red-600 text-white rounded-lg">
-                      Cancel
-                    </button>
+                    {order.status == "Created" && (
+                      <>
+                        <button
+                          onClick={() => updateStatus("Cancelled", order._id)}
+                          className="px-3 py-2 bg-red-500 hover:bg-red-600 text-white rounded-lg"
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          onClick={() => updateStatus(order.status, order._id)}
+                          className="px-3 py-2 bg-yellow-500 hover:bg-yellow-600 text-white rounded-lg"
+                        >
+                          Mark as shipped
+                        </button>
+                      </>
+                    )}
 
-                    <button className="px-3 py-2 bg-yellow-500 hover:bg-yellow-600 text-white rounded-lg">
-                      Mark as shipped
-                    </button>
+                    {order.status == "Shipped" && (
+                      <button
+                        onClick={() => updateStatus(order.status, order._id)}
+                        className="px-3 py-2 bg-green-500 hover:bg-green-600 text-white rounded-lg"
+                      >
+                        Mark as delivered
+                      </button>
+                    )}
 
-                    <button className="px-3 py-2 bg-green-500 hover:bg-green-600 text-white rounded-lg">
-                      Mark as delivered
-                    </button>
+                    {["Delivered", "Cancelled"].includes(order.status) && (
+                      <span className="font-bold text-red-500">
+                        No Actions available
+                      </span>
+                    )}
                   </td>
                 </tr>
               );
