4329fe357932fd752c667067da0c39f99ef67766
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-26 14:04:51 +0600

    Show products on homescreen;

--- src/components/Part/Part.jsx
+++ src/components/Part/Part.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Part = ({ className }) => {
+const Part = ({ className, part }) => {
   return (
     <div className={`shadow-lg p-4 rounded-lg relative border-2 ${className}`}>
       <img
@@ -12,31 +12,32 @@ const Part = ({ className }) => {
 
       <div className="mt-5">
         <h1 className="text-xl sm:text-2xl font-bold text-zinc-700">
-          Awesome mouse pro
+          {part.name}
         </h1>
         <p className="text-xs sm:text-sm text-zinc-500">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-          maxime.
+          {part.short_description}
         </p>
 
         <div className="grid grid-cols-2 gap-2 text-center mt-2">
           <div className="bg-zinc-300 cursor-pointer text-zinc-800 py-2 rounded-md">
             <span className="text-xs sm:text-sm md:text-base">
-              Available : <span className="font-bold text-zinc-600">1000</span>
+              Available :
+              <span className="font-bold text-zinc-600">{part.quantity}</span>
             </span>
           </div>
 
           <div className="bg-zinc-300 cursor-pointer text-zinc-800 py-2 rounded-md">
             <span className="text-xs sm:text-sm md:text-base">
-              Min Order : <span className="font-bold text-zinc-600">50</span>
+              <span>Min Order :</span>
+              <span className="font-bold text-zinc-600">{part.min_order}</span>
             </span>
           </div>
 
           <Link
             className="bg-black text-zinc-200 py-2 rounded-md col-span-2"
-            to="/shop/something"
+            to={`/shop/${part._id}`}
           >
-            Book Now
+            Order Now
           </Link>
         </div>
       </div>
--- src/sections/Parts/Parts.jsx
+++ src/sections/Parts/Parts.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import Container from '../../components/Layout/Container'
-import Part from '../../components/Part/Part'
+import React from "react";
+import { useQuery } from "react-query";
+import Container from "../../components/Layout/Container";
+import Part from "../../components/Part/Part";
+import { API_BASE } from "../../pages/config";
 
 const Parts = () => {
-  return (
-    <div className='border-0 border-t-2'>
-      <Container className='py-20'>
-        <h1 className='text-3xl text-center font-bold'>Tools 💻 Parts</h1>
+  const {
+    data: parts,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery("products", () =>
+    fetch(`${API_BASE}/products`).then((data) => data.json())
+  );
 
-        <div className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
-          <Part />
-          <Part />
-          <Part className={`
-            sm:last:grid sm:last:grid-cols-2 sm:last:col-span-2 sm:last:gap-5 
-            md:last:grid md:last:grid-cols-1 md:last:col-span-1 md:last:gap-0
-          `} />
+  return (
+    <div className="border-0 border-t-2">
+      <Container className="py-20">
+        <h1 className="text-3xl text-center font-bold">Tools 💻 Parts</h1>
+
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+          {parts?.slice(0, 3)?.map((part, index) => {
+            return (
+              <Part
+                part={part}
+                key={index}
+                className={
+                  index == 2
+                    ? `
+                      sm:last:grid sm:last:grid-cols-2 sm:last:col-span-2 sm:last:gap-5 
+                      md:last:grid md:last:grid-cols-1 md:last:col-span-1 md:last:gap-0
+                    `
+                    : ""
+                }
+              />
+            );
+          })}
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Parts
+export default Parts;
